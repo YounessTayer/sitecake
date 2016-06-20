4833fcbf38430dd91279fe70fc5b73847315057d
Predrag Leka <predragleka@gmail.com>
2016-06-20 10:48:12 +0200

    Build fixed when checking existing build dir * RendomDirectory test separated to have two test for existing and newly created random dir * src dir in client removed

--- server/tests/Sitecake/Filesystem/RandomDirectoryTest.php
+++ server/tests/Sitecake/Filesystem/RandomDirectoryTest.php
@@ -8,13 +8,22 @@ class RandomDirectoryTest extends \PHPUnit_Framework_TestCase {
         \Mockery::close();
     }
 
-	public function testHandle() {
+	public function testHandleExisting() {
 		$fs = \Mockery::mock('League\Flysystem\Filesystem');
 
 		$fs->shouldReceive('listPatternPaths')
 			->with('d1', '/^.*\/r[0-9a-f]{13}$/')
 			->andReturn(array('d1/r1234567890123'))
 			->once();
+
+		$plugin = new RandomDirectory();
+		$plugin->setFilesystem($fs);
+
+		$this->assertEquals('d1/r1234567890123', $plugin->handle('d1'));
+	}
+
+	public function testHandleNew() {
+		$fs = \Mockery::mock('League\Flysystem\Filesystem');
 
 		$fs->shouldReceive('listPatternPaths')
 			->with('d2', '/^.*\/r[0-9a-f]{13}$/')
@@ -24,12 +33,11 @@ class RandomDirectoryTest extends \PHPUnit_Framework_TestCase {
 		$fs->shouldReceive('ensureDir')
 			->with('/^d2\/r[0-9a-f]{13}$/')
 			->andReturn(true)
-			->once();			
+			->once();
 
 		$plugin = new RandomDirectory();
 		$plugin->setFilesystem($fs);
 
-		$this->assertEquals('d1/r1234567890123', $plugin->handle('d1'));
 		$this->assertEquals(1, preg_match('/^d2\/r[0-9a-f]{13}$/', $plugin->handle('d2')));
 	}
 }
